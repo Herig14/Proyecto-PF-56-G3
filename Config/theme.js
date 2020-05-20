d1d451a71e30368ccd1bf10e20146c96154bdb77
Herig Recalde <herig14@gmail.com>
2020-05-20 02:39:44 -0500

    Modificación del tema de colors

--- Config/theme.js
+++ Config/theme.js
@@ -1,16 +1,18 @@
 const colors = require('colors/safe')
 
 colors.setTheme({
+    titulos: ['blue', 'bold'],
     silly: 'rainbow',
     input: 'grey',
     verbose: 'cyan',
     prompt: 'grey',
-    info: 'green',
-    data: 'grey',
+    info: 'brightGreen',
+    data: 'brightMagenta',
     help: 'cyan',
     warn: 'yellow',
     debug: 'blue',
     error: 'red'
+
 });
 module.exports = {
     colors
