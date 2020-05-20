04112255887aaf3a0cd18dec16a051d324657645
Herig Recalde <herig14@gmail.com>
2020-05-20 02:27:22 -0500

    Optimización de servidor

--- Controlador/server.js
+++ Controlador/server.js
@@ -5,12 +5,11 @@ const port = 3000;
 function start(hmtlcode) {
     const server = http.createServer((req, res) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
+        // res.setHeader('Content-Type', 'text/html');
         res.end(hmtlcode);
     });
-    server.listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);
-    });
+    server.listen(port, hostname);
+    return `Ejecución del servidor en: http://${hostname}:${port}/\nctrl + c para detener el proceso`
 }
 module.exports = {
     start
