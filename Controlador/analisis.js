b8e9f5ea3b845fdaa63a1a77c21478bc82aee554
Herig Recalde <herig14@gmail.com>
2020-05-20 02:24:14 -0500

    Manejo de errores y validaciones

--- Controlador/analisis.js
+++ Controlador/analisis.js
@@ -1,4 +1,5 @@
 const getJSON = require("../modelo/importCSV").getJSON
+const lookup = require('country-data').lookup;
 let total
 async function buscarPais(pais, year, json) {
     var jsonOB = json
@@ -10,7 +11,7 @@ async function buscarPais(pais, year, json) {
             break;
         }
     }
-    return { name: aux['Country Name'], suscripciones: aux[year], anio: year }
+    return { name: aux['Country Name'], suscripciones: aux[year], anio: year, code: pais }
 
 }
 
@@ -55,6 +56,15 @@ async function updown5(pais, year, json) {
     }
     jsonOB.sort(GetSortOrder(year)); //Pass the attribute to be sorted on
     jsonOB = jsonOB.slice(0, total);
+    let jsonTemp = []
+    for (var item in jsonOB) {
+        temp = jsonOB[item]['Country Code']
+        let codeP = lookup.countries({ alpha3: temp })[0];
+        if (codeP !== undefined) {
+            jsonTemp.push(jsonOB[item])
+        }
+    }
+    jsonOB = jsonTemp
     top5 = jsonOB.slice(0, 5);
     let indexP = 0
     for (var item in jsonOB) {
@@ -75,28 +85,47 @@ async function updown5(pais, year, json) {
         final = indexP
     }
     up5 = jsonOB.slice(inicio, final);
-    return { top: top5, up: up5, down: down5 }
+    return { top: top5, up: up5, down: down5, index: indexP }
 }
 
 async function analizar(pais, year, csvpath) {
-    pais = pais.toUpperCase()
-    let jsonOB = await getJSON(csvpath).then().catch(err => console.log(err.message));
-    let paisOB = await buscarPais(pais, year, jsonOB)
-    let media = await mediaAnual(year, jsonOB)
-    let comparacion
-    if (paisOB.suscripciones > media) {
-        comparacion = `El valor de las suscripciones de ${paisOB.name} es mayor a la media mundial en el año ${year} `
-    } else if (paisOB.suscripciones < media) {
-        comparacion = `El valor de las suscripciones de ${paisOB.name} es menor a la media mundial en el año ${year} `
-    } else if (paisOB.suscripciones <= 0) {
-        comparacion = `El país ${paisOB.name} no tiene resgistradas suscripciones en el año ${year}  `
-    } else {
-        comparacion = `El valor de las suscripciones de ${paisOB.name} es igual a la media mundial en el año ${year} `
+    let errorCode = 'El parámetro country debe ser un código ISO 3166 ALPHA-3.'
+    try {
+        pais = pais.toUpperCase()
+        let codeP = lookup.countries({ alpha3: pais })[0];
+        if (codeP == undefined) {
+            throw new Error(errorCode)
+        }
+    } catch (error) { //ISO 3166 ALPHA-3
+        throw new Error(errorCode)
     }
-    let st = await updown5(pais, year, jsonOB).then().catch(err => console.log(err.message))
-    return [paisOB, media, comparacion, st]
-
-
+    let msg
+    let jsonOB = await getJSON(csvpath).then().catch(err => msg = err.message);
+    try {
+        let paisOB = await buscarPais(pais, year, jsonOB)
+        let media = await mediaAnual(year, jsonOB)
+        let comparacion
+            //Verificar existencia de registros
+        if (!Number.isInteger(year) || year < 1960) {
+            msg = `El valor del parámetro year deber ser un número mayor o igual a 1960`
+            throw new Error(msg)
+        } else if (isNaN(media)) {
+            msg = `No existen registros para el año ${year}`
+            throw new Error(msg)
+        } else if (paisOB.suscripciones > media) {
+            comparacion = `Las suscripciones de ${paisOB.name} son mayores a la media mundial \n en el año ${year} `
+        } else if (paisOB.suscripciones < media && paisOB.suscripciones > 0) {
+            comparacion = `Las suscripciones de ${paisOB.name} son menores a  la media mundial \n en el año ${year} `
+        } else if (paisOB.suscripciones <= 0) {
+            comparacion = `El país ${paisOB.name} no tiene suscripciones resgistradas  en el año ${year}  `
+        } else {
+            comparacion = `Las suscripciones de ${paisOB.name} son iguales a la media mundial \n en el año ${year} `
+        }
+        let st = await updown5(pais, year, jsonOB).then().catch(err => console.log(err.message))
+        return [paisOB, media, comparacion, st]
+    } catch (error) {
+        throw new Error(msg)
+    }
 }
 module.exports = {
     mediaAnual,
